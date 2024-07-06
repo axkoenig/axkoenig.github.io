2ef91a95709704a9c63cbfec1342778228c13379
Alexander Koenig <awckoenig@gmail.com>
2024-07-06 21:00:06 +0200

    Basic collapsable images

--- new_website/scripts.js
+++ new_website/scripts.js
@@ -1,4 +1,3 @@
-
 function adjustSidebarHeight() {
     const sidebarRows = document.querySelectorAll('.sidebar .row');
     const contentRows = document.querySelectorAll('.content .row');
@@ -13,3 +12,21 @@ function adjustSidebarHeight() {
 
 window.addEventListener('load', adjustSidebarHeight);
 window.addEventListener('resize', adjustSidebarHeight);
+
+function expandRows() {
+    const hiddenItems = document.querySelectorAll('.grid-item.hidden');
+    hiddenItems.forEach(item => item.classList.remove('hidden'));
+    document.getElementById('expandButton').classList.add('hidden');
+    document.getElementById('collapseButton').classList.remove('hidden');
+    adjustSidebarHeight();
+}
+
+function collapseRows() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    for (let i = 2; i < gridItems.length; i++) {
+        gridItems[i].classList.add('hidden');
+    }
+    document.getElementById('expandButton').classList.remove('hidden');
+    document.getElementById('collapseButton').classList.add('hidden');
+    adjustSidebarHeight();
+}
