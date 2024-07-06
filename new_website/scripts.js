d937f30218920677ca947dccb752ef00d97f5986
Alexander Koenig <awckoenig@gmail.com>
2024-07-06 22:37:37 +0200

    Fix bug

--- new_website/scripts.js
+++ new_website/scripts.js
@@ -4,8 +4,10 @@ function adjustSidebarHeight() {
 
     for (let i = 1; i < sidebarRows.length; i++) { // Start from 1 to skip the #home row
         if (contentRows[i - 1]) { // Match the index offset
-            const contentHeight = contentRows[i - 1].offsetHeight;
-            sidebarRows[i].style.height = contentHeight < parseInt(getComputedStyle(document.documentElement).getPropertyValue('--row-min-height')) ? '150px' : contentHeight + 'px';
+            const contentHeight = contentRows[i - 1].getBoundingClientRect().height;
+            const minHeight = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--row-min-height'));
+
+            sidebarRows[i].style.height = contentHeight < minHeight ? '150px' : `${contentHeight}px`;
         }
     }
 }
